75fa9e7b20a91dad28c09a2b9c0aaa66fb9b4bc5
shaban <shabanpargal20@gmail.com>
2025-01-27 01:08:44 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.main-img')
+let body = document.body
+
+function phones(phone) {
+    img.src = phone ;
+}
+
+function colors(color) {
+    body.style.background = color ;
+}
